f0ac7d8b76037a8c9c2a0041e1579ce2c7b2ca73
John Furtado <jf8846@gmail.com>
2019-03-02 18:53:07 -0500

    get employee by id

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,6 +1,13 @@
-var app = angular.module('app',[]);
+var app = angular.module('app', []);
 
 app.service('EmployeeCRUDService', [ '$http', function($http) {
 	 
+	this.getUser = function getEmployee(employeeId) {
+		
+		return $http({
+			method: 'GET',
+			url: 'api/employees/' + employeeId
+		});
+	}
     
 } ]);
